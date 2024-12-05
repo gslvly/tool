986339ebdccbe4bea7ecf8a30550db67b6b275f9
gs <605661239@qq.com>
2024-12-05 16:02:17 +0800

    save

--- opener.ts
+++ opener.ts
@@ -7,12 +7,17 @@ export type IPos = {
 }
 type IWin = (Window & { channel: BroadcastChannel; domWrapper: HTMLDivElement }) | undefined
 
+
+/**
+ * @param onClose 只有浏览器原生关闭按钮会触发它
+ * @description 父窗口不可见时（document.hidden === true），js无法运行，必须同步禁止使用子窗口
+ */
 export const createOpenerManger = (opt: {
-  onClose: (v: IPos) => void
-  getPos: () => IPos | undefined
+  onClose?: (v: IPos) => void
+  getPos?: () => IPos | undefined
 }) => {
   let w: IWin
-  let closeTimeout: number
+  const key = Date.now() + ''
 
   const cloneNode = (node: HTMLElement | SVGSymbolElement) => {
     return w!.document.importNode(node, true)
@@ -20,31 +25,28 @@ export const createOpenerManger = (opt: {
   const moveNode = (node: HTMLElement) => {
     return w!.document.adoptNode(node)
   }
-  const open = async (dom: HTMLElement) => {
-    clearTimeout(closeTimeout)
-    closeTimeout = -1
-
+  const open = (dom: HTMLElement) => {
     if (w && w.document.contains(dom)) {
       w.focus()
-      return
+      return true
     }
 
-    const oldPos = opt.getPos()
+    const oldPos = opt.getPos?.()
     if (w) {
       w.domWrapper.innerHTML = ''
       w.domWrapper.appendChild(moveNode(dom))
       w.focus()
       // 避免注册多个事件
-      return
+      return true
     }
 
     w = window.open(
       '',
-      'detail',
+      key,
       `popup=true,width=${oldPos?.width || 500},height=${oldPos?.height || 600}`
     ) as IWin
 
-    if (!w) return
+    if (!w) return false
 
     console.log('move', oldPos)
     oldPos && w.moveTo(oldPos.x, oldPos.y)
@@ -52,65 +54,73 @@ export const createOpenerManger = (opt: {
     const style = document.querySelectorAll('style')
     style.forEach((it) => w!.document.head.appendChild(cloneNode(it)))
 
+    const links = document.querySelectorAll('link[rel="stylesheet"]')
+    links.forEach((it) => w!.document.head.appendChild(cloneNode(it)))
+
     const symbolIcon = document.querySelectorAll('symbol')
     const svg = w.document.createElementNS('http://www.w3.org/2000/svg', 'svg')
     svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
     svg.setAttribute('xmlns:link', 'http://www.w3.org/1999/xlink')
 
-    w.domWrapper = document.createElement('div')
+    symbolIcon.forEach((it) => svg.appendChild(cloneNode(it)))
+    // 必须渲染在body中才会生效，否则复杂svg可能不会渲染成功
+    w.document.body.insertBefore(svg, w.document.body.firstChild)
+
+    w.domWrapper = w.document.createElement('div')
     w.domWrapper.appendChild(moveNode(dom))
     w.document.body.appendChild(w.domWrapper)
-
-    symbolIcon.forEach((it) => svg.appendChild(cloneNode(it)))
-
-    w.document.body.insertBefore(svg, w.document.body.firstChild)
     // 传递事件
     // select使用mouseup收起来
     w.addEventListener('mouseup', (e) => {
       const isSelectOrInput = (it: EventTarget) =>
-        (it as HTMLElement).className?.includes?.('el-input') || (it as HTMLElement).className?.includes?.('el-select')
+        (it as HTMLElement).className?.includes?.('el-input') ||
+        (it as HTMLElement).className?.includes?.('el-select')
 
       if (e.composedPath().some(isSelectOrInput)) return
       document.dispatchEvent(new Event('mouseup'))
+      window.dispatchEvent(new Event('mouseup'))
     })
     // popper使用click事件收起来
     w.addEventListener('click', (e) => {
-      if (e.composedPath().some((it) => (it as HTMLElement).className?.includes?.('el-popover'))) return
+      console.log('click')
+      if (e.composedPath().some((it) => (it as HTMLElement).className?.includes?.('el-popover'))) {
+        return
+      }
       document.dispatchEvent(new Event('click'))
+      window.dispatchEvent(new Event('click'))
     })
 
     w.onbeforeunload = (e) => {
       if (!w) return
-      const pos = {
-        height: w!.outerHeight,
-        width: w!.outerWidth,
-        x: w!.screenX,
-        y: w!.screenY
-      }
-      opt?.onClose?.(pos)
+      opt?.onClose?.(getPos()!)
+    }
+    return true
+  }
+
+  const getPos = () => {
+    if (!w) return null
+    return {
+      height: w.outerHeight,
+      width: w.outerWidth,
+      x: w.screenX,
+      y: w.screenY
     }
   }
 
-  const close = (notice = true) => {
+  /**不触发onClose */
+  const close = () => {
     if (!w) return
-    if (notice) {
-      closeTimeout = window.setTimeout(() => {
-        console.log('close')
-        w?.close()
-        w = undefined
-      }, 200)
-    } else {
-      w.onbeforeunload = null
-      w.close()
-      w = undefined
-    }
+    console.log('close')
+    w.onbeforeunload = null
+    w.close()
+    w = undefined
   }
-
-  window.addEventListener('beforeunload', () => {
-    w?.close()
-  })
 
   return {
+    get win() {
+      return w
+    },
+    getPos,
     open,
     close
   }
